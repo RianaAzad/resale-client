e2748ac4278443621f3600ec7a34a17d88b6e225
Riana Azad <Riana1996>
2022-11-25 16:48:32 +0600

    signup page

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,31 +1,43 @@
 import React from 'react';
-import { useState } from "react";
 import { useForm } from "react-hook-form";
-import login from '../../assets/Login.png'
+import { Link } from 'react-router-dom';
 
 
 const Login = () => {
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
-  
-    console.log(watch("example")); // watch input value by passing the name of it
-  
-    return (
-      
-        <div className='lg:h-[600px] mx-auto my-10 lg:my-44 md:w-1/4 p-10 md:p-0'>
-        <h1 className='text-3xl text-primary font-serif text-center'>Please Log In!</h1>
-        <form onSubmit={handleSubmit(onSubmit)} className='grid gap-3 grid-cols-1 mx-auto my-5'>
-        <input type="text" placeholder="Type here" className="input input-bordered input-primary w-full" 
-        {...register("name")} />
-        <input type="text" placeholder="Type here" className="input input-bordered input-primary w-full" 
-         {...register("email", { required: true })} />
-        <input type="text" placeholder="Type here" className="input input-bordered input-primary w-full" 
-         {...register("password", { required: true })} />
-        
-        <input className='btn btn-primary' type="submit" />
+  const { register, formState: { errors }, handleSubmit } = useForm();
+
+  const handleLogin = data => {
+    console.log(data)
+  }
+
+  return (
+    <div className='lg:h-[600px] mx-auto my-10 lg:my-44 md:w-1/4 p-10 md:p-0'>
+      <h1 className='text-3xl text-primary font-serif text-center'>Please Log In!</h1>
+
+      <form onSubmit={handleSubmit(handleLogin)} className='grid gap-1 grid-cols-1 mx-auto my-5'>
+
+        <label className='label'><span className='label-text'>Email</span></label>
+        <input type="text" placeholder="Your Email here" className="input input-bordered input-primary w-full"
+          {...register("email", { required: "Email is required" })} />
+        {errors.email?.type === 'required' && <p className='text-red-600'>{errors.email?.message}</p>}
+
+        <label htmlFor="password">Password</label>
+        <input type="password" placeholder="Password" className="input input-bordered input-primary w-full" id="password"
+          {...register("password", {
+            required: "Password is required", minLength: {
+              value: 6,
+              message: "Minimum password length is 6"
+            }
+          })} />
+        {errors.password && <span role="alert" className='text-red-600'>{errors.password.message}</span>}
+
+        <input className='btn btn-primary mt-4' type="submit" value="Log In" />
       </form>
-      </div>
-      
-    );
+      <p className='text-center'>New to Buy & Sale? <Link className='text-secondary text-lg font-bold font-sans' to='/signup'>Create new account</Link></p>
+      <div className="divider">OR</div>
+      <button className='btn btn-outline w-full'>Google</button>
+    </div>
+
+  );
 };
 export default Login;
--- src/Pages/Login/Signup.js
+++ src/Pages/Login/Signup.js
@@ -1,10 +1,55 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 
 const Signup = () => {
+    const { register, formState: { errors }, handleSubmit } = useForm();
+
+    const handleSignup = data => {
+        console.log(data)
+    }
     return (
-        <div>
-            
+        <div className='lg:h-[600px] mx-auto my-10 lg:my-44 md:w-1/4 p-10 md:p-0'>
+            <h1 className='text-3xl text-primary font-serif text-center'>Sign Up for free!</h1>
+            <form onSubmit={handleSubmit(handleSignup)}>
+                    <div className="form-control w-full">
+                        <label className="label"><span className="label-text">Name</span></label>
+                        <input type="text" {...
+                            register("name", {required: "Please provide your name"})
+                        } className="input input-bordered w-full" />
+                         {errors.name && <p className='text-red-600'>{errors.name.message}</p>}
+                    </div>
+
+                    <div className="form-control w-full">
+                        <label className="label"><span className="label-text">Email</span></label>
+                        <input type="email" {...
+                        register("email", {required: "Email is required"})
+                        } className="input input-bordered w-full" />
+                        {errors.email && <p className='text-red-600'>{errors.email.message}</p>}
+                    </div>
+
+                    <div className="form-control w-full">
+
+                        <label className="label" htmlFor="password">Password</label>
+                        <input type="password" className="input input-bordered w-full"
+                        {...register("password", {required: "Password is required",
+                        minLength: {value: 6, message: "Password must be 6 characters long"},
+                        pattern: {value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/, 
+                        message: "Password must have 1 Upper case, 1 special character and 1 number"}
+                        })} />
+                        {errors.password && <p className='text-red-600'>{errors.password.message}</p>}
+                        {
+                            !errors.password && <p className='text-green-600 font-bold'>Wow, Strong password!</p>
+                        }
+                    </div>
+                    <input className='btn btn-accent w-full mt-5' value="Sign Up" type="submit" />
+                    
+                </form>
+            <p className='text-center'>Already have an account? <Link className='text-secondary text-lg font-bold font-sans' to='/login'>Log In</Link></p>
+            <div className="divider">OR</div>
+            <button className='btn btn-outline w-full'>Google</button>
         </div>
+
     );
 };
 
